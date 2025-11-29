98cc9302e6ad193ba63e39d939ef3d7ebf46a9ef
Lihui Gao <Lihuigao21@gmail.com>
2025-11-29 22:50:06 +0800

    Fix setup.py metadata and bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 25 17:39:40 2024
-
-@author: ASUS
+setup.py for simplemathglh
 """
 
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README.md as long description
+this_dir = Path(__file__).parent
+long_description = (this_dir / "Readme.md").read_text(encoding="utf-8")
 
 setup(
     name="simplemathglh",
-    version="0.1.0",
-    description="A simple library for test.",
+    version="0.1.1",  
+    description="A simple library for basic math operations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     author="Lihui Gao",
     author_email="Lihuigao21@gmail.com",
-    url="test",
+
+    url="https://github.com/Lihuigao21/simplemathglh",   
+
     packages=find_packages(),
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
     python_requires=">=3.6",
 )
